cbf78b45357aaeb4b5af10fe5298c758739f2536
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-09 15:30:20 +1100

    performed rollback, removed null, added default fields to students table

--- db/migrate/20210308053346_create_students.rb
+++ db/migrate/20210308053346_create_students.rb
@@ -1,12 +1,13 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :first_name
-      t.string :middle_name
-      t.string :last_name
-      t.string :email
-      t.date :birth_date
-      t.string :gender
+      t.string :title, null: false, default: ""
+      t.string :first_name, null: false, default: ""
+      t.string :middle_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :email, null: false
+      t.date :birth_date, null: false
+      t.string :gender, null: false
 
       t.timestamps
     end
--- db/migrate/20210309025913_add_title_to_students.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToStudents < ActiveRecord::Migration
-  def change
-    add_column :students, :title, :string
-  end
-end
