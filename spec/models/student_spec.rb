77f93a267313ba914682b242dd84b292e049f304
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-10 10:33:29 +1100

    rspec config, added full_name test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Student, type: :model do
+
+  it "is valid with all attributes" do
+    stu = create(:student)
+    expect(stu).to be_valid
+  end
+  
+  it "returns students full name as a string" do
+    stu = create(:student)
+    expect(stu.full_name).to eq("#{stu.title} #{stu.first_name} #{stu.middle_name} #{stu.last_name}")
+  end
+
 end
