569c8191994715abffc8a51c9e3b7de16f365737
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-16 09:15:49 +1100

    added title table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20210308053346) do
+ActiveRecord::Schema.define(:version => 20210315214251) do
 
   create_table "students", :force => true do |t|
     t.string   "title",       :default => "", :null => false
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(:version => 20210308053346) do
     t.string   "gender",                      :null => false
     t.datetime "created_at",                  :null => false
     t.datetime "updated_at",                  :null => false
+  end
+
+  create_table "titles", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
