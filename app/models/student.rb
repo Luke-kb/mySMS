e8e4a577d056e0efbc8a825929c8ae8852d7c92d
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-15 12:02:55 +1100

    added nil validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,11 @@
 class Student < ActiveRecord::Base
   attr_accessible :title, :birth_date, :email, :first_name, :gender, :last_name, :middle_name
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :birth_date, presence: true
-  validates :gender, presence: true
+  validates :first_name, presence: true, exclusion: [nil]
+  validates :last_name, presence: true, exclusion: [nil]
+  validates :email, presence: true, uniqueness: true, exclusion: [nil]
+  validates :birth_date, presence: true, exclusion: [nil]
+  validates :gender, presence: true, exclusion: [nil]
 
   def full_name
     [title, first_name, middle_name, last_name].join(' ')
