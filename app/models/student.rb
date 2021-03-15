3ba3d976d773b5e25dce6fa02910e1bf04933a73
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-16 09:09:57 +1100

    added belongs_to relation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ActiveRecord::Base
   attr_accessible :title, :birth_date, :email, :first_name, :gender, :last_name, :middle_name
 
+  belongs_to :title
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
