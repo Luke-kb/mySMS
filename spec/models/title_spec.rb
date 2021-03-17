b2d8d3b0808558d623855463fe913537a6704e57
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-18 10:43:42 +1100

    updated title and student spec with association

--- spec/models/title_spec.rb
+++ spec/models/title_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Title, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject { title }
+  context 'with valid attributes' do 
+    let(:title) { build(:title, name: "Dr.") }
+
+    it { is_expected.to be_valid }
+
+  end
 end
