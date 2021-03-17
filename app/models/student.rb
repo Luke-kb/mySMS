26bfd152d84f9e8d3d5d882718c3a417f8cbbe64
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-18 10:42:14 +1100

    updated full_name method. updated views to pull from title model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,9 @@
 class Student < ActiveRecord::Base
-  attr_accessible :title, :birth_date, :email, :first_name, :gender, :last_name, :middle_name
+  attr_accessible :title_id, :birth_date, :email, :first_name, :gender, :last_name, :middle_name
 
   belongs_to :title
 
+  validates :title, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
@@ -10,7 +11,7 @@ class Student < ActiveRecord::Base
   validates :gender, presence: true
 
   def full_name
-    [title, first_name, middle_name, last_name].join(' ')
+    [title.name, first_name, middle_name, last_name].join(' ')
   end
 
 end
