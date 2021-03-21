384227d22f29e5e4f289b216b21df8637e9ea929
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-22 09:13:34 +1100

    validation added to course model. inconsistent indentation fixed in form.css

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,11 @@
 class Course < ActiveRecord::Base
   attr_accessible :allocation, :description, :end_date, :name, :number_of_semesters, :start_date
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :number_of_semesters, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :allocation, presence: true
+
 end
