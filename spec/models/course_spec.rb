f55a5602110ee3739f205e73b2826c0564d89ee4
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-23 10:23:18 +1100

    added shoulda-matchers. updated course_spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
+  
   subject { course }
   context 'with valid attributes' do
-    let(:course) { build(:course, name: "course1", description: "description", number_of_semesters: 1, start_date: "2021-01-20", end_date: "2020-05-20", allocation: 10) }
+     let!(:course) { build(:course, name: "course1", description: "description", number_of_semesters: 1, start_date: "2021-01-20", end_date: "2020-05-20", allocation: 10) }
   
     it "is valid" do
-      expect(subject).to be_valid
+      subject
+      expect(course).to be_valid
     end
   end
 
   context "with invalid attributes" do
-    let(:course) { build(:course, name: nil) }
+    let!(:course) { build(:course, name: nil) }
+    
+    it { is_expected.to validate_presence_of(:name) }
 
-    it "is valid" do
-      expect(subject).to_not be_nil
+    it "is not valid" do
+      course.valid?
+      expect(course.errors[:name]).to include("can't be blank")
     end
   end
   
