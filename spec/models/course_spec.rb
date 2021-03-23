f085ba08e60d75001bbfd8705b261e018bce3dd2
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-03-23 11:02:53 +1100

    fixed indentation

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Course, type: :model do
   
   subject { course }
   context 'with valid attributes' do
-     let!(:course) { build(:course, name: "course1", description: "description", number_of_semesters: 1, start_date: "2021-01-20", end_date: "2020-05-20", allocation: 10) }
+    let!(:course) { build(:course, name: "course1", description: "description", number_of_semesters: 1, start_date: "2021-01-20", end_date: "2020-05-20", allocation: 10) }
   
     it "is valid" do
       subject
