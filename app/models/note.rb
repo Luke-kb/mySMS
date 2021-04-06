7b3df9e0adc50f31064739f8a50f193e5c796283
Luke_KB <luke.keanan-brown@AU-00-ML-0014.local>
2021-04-06 16:42:55 +1000

    added note validation. form error styling.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,7 @@ class Note < ActiveRecord::Base
   attr_accessible :content, :title
 
   belongs_to :notable, polymorphic: true
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Mysms::Application.routes.draw do
-  
+
   resources :students do
     resources :notes, module: :students
   end
   
   resources :courses
+  
   resources :institutes
   
   root to: "students#index"
